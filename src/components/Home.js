34f14b72dc151fa7a4e81748dff53c4055d5f48f
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-08 14:07:38 -0300

    structuring home component and links to profile, comics and characters

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
-// import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 function Home () {
+  const history = useHistory();
+
+  const handleProfile = () => {
+    history.push('/profile')
+  }
+  const handleComics = () => {
+    history.push('/comics')
+  }  
+  const handleCharacters = () => {
+    history.push('/characters')
+  }
+
   return (
-    <div>
-      Home
+    <div className='container bg-light'>
+      <h1 className='text-center text-black my-5'>Marvel App</h1>
+        <div className='d-flex justify-content-center'>
+          <div className='btn-group-vertical my-3'>
+            <button onClick={() => handleProfile()} type='button' className='btn-lg btn btn-dark my-3'>Perfil</button>
+            <button onClick={() => handleComics()} type='button' className='btn-lg btn btn-dark my-3'>Comics</button>
+            <button onClick={() => handleCharacters()} type='button' className='btn-lg btn btn-dark my-3'>Characters</button>
+          </div>
+        </div>
     </div>
   );
-};
+}
 
 export default Home;
