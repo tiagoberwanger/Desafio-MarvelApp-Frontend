1de3145f067f5549d93d6368b2e7fd8f0f87b742
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-08 22:36:49 -0300

    structuring comics component basis and axios request

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -1,12 +1,34 @@
-import React from 'react';
-// import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+// import { Link, useHistory } from 'react-router-dom';
+const axios = require('axios');
 
 function Comics () {
+  // const history = useHistory();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    axios({
+      method: 'get',
+      url: 'http://localhost:3001/comics',
+      headers: {'authorization': JSON.parse(localStorage.getItem('token'))}, 
+    })
+      .then((response) => {
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((err) =>{
+        console.log(localStorage.getItem('token'))
+        console.log(err.message);
+      })
+  }, [])
+
   return (
-    <div>
-      Comics
+    loading ? <p>Loading...</p> : (
+    <div className="container">
+      Comics Cards
     </div>
-  );
-};
+  ));
+}
 
 export default Comics;
