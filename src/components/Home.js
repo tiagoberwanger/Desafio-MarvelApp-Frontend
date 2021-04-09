916266bf37492ea120eea4ea16714831a5fc1182
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-09 12:29:47 -0300

    implementing logout and localStorage clear

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,14 +14,22 @@ function Home () {
     history.push('/characters')
   }
 
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div className='container bg-light'>
       <h1 className='text-center text-black my-5'>Marvel App</h1>
-        <div className='d-flex justify-content-center'>
-          <div className='btn-group-vertical my-3'>
-            <button onClick={() => handleProfile()} type='button' className='btn-lg btn btn-dark my-3'>Perfil</button>
-            <button onClick={() => handleComics()} type='button' className='btn-lg btn btn-dark my-3'>Comics</button>
-            <button onClick={() => handleCharacters()} type='button' className='btn-lg btn btn-dark my-3'>Characters</button>
+        <div className='text-center'>
+          <button onClick={() => handleProfile()} type='button' className='btn-lg btn btn-dark my-3'>Perfil</button>
+          <div className='d-flex justify-content-center'>
+            <button onClick={() => handleComics()} type='button' className='btn-lg btn btn-dark p-3 mr-3 my-3'>Comics</button>
+            <button onClick={() => handleCharacters()} type='button' className='btn-lg btn btn-dark p-3 my-3 ml-3'>Characters</button>
+          </div>
+          <div className="my-5">
+            <button onClick={() => handleLogout()} type='button' className='btn-lg btn btn-dark my-3'>Sair</button>
           </div>
         </div>
     </div>
